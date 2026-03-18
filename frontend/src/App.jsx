7f28cb1fedc4851569c18bd5ff1654d5085ef999
Jairo Nacurena <r1067776@student.thomasmore.be>
2026-03-18 23:52:31 +0100

    deploy with azure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="container mt-5 text-center">
-      <h1 className="text-primary ">🚀 SKIL2 Portfolio Base</h1>
+      <h1 className="text-primary ">🚀 SKIL2 Portfolio Base Template 🚀</h1>
       <p className="lead">React + Bootstrap</p>
     </div>
   );
