37820b783aefb23f9e0eaa83858aa27686a0a98a
Jairo Nacurena <r1067776@student.thomasmore.be>
2026-03-19 12:53:24 +0100

    test again portolio

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ function App() {
     <div className="container mt-5 text-center">
       <h1 className="text-primary ">🚀 SKIL2 Portfolio Base</h1>
       <p className="lead">React + Bootstrap</p>
+      <hr className="my-4" />
+      <p className='text-'>This is a test of my portfolio</p>
     </div>
   );
 }
